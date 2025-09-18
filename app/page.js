609f63ba2076e14e4704856dfa06a0ab9ed7c496
Kiera Johnson <kj9isawesome@gmail.com>
2025-09-18 09:06:52 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
         <h1>CPRG306: Web Development 2 - Assignments</h1>
       </header>
       <Link href="week-2">Week 2</Link> <br></br>
-      <Link href="week-3">Week 3</Link>
     </main>
   )
 }
