4d08403cb6c7e2eddd1d2d0f7997d097a64a27cf
Kiera Johnson <kj9isawesome@gmail.com>
2025-09-18 08:58:21 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo(){
     return (
         <div>
             <h1>Kiera Johnson</h1>
-            <Link href="https://github.com/kj7639/cprg306-assignments.git">GitHub Repo Link</Link>
+            <Link href="https://github.com/kj7639/cprg306-assignments.git">Kiera GitHub Repo Link</Link>
         </div>
     );
 }
