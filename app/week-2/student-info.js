0da03e79a9059d8ad675eeb060d81ef8a9f3066f
Kiera Johnson <kj9isawesome@gmail.com>
2025-09-18 08:04:57 -0600

    updated week-2 files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function StudentInfo(){
+    return (
+        <div>
+            <h1>Kiera Johnson</h1>
+            <Link href="https://github.com/kj7639/cprg306-assignments.git">GitHub Repo Link</Link>
+        </div>
+    );
+}
